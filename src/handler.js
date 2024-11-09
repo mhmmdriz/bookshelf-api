546b5fbb1bfb463577e31af4c97fbf8638b42179
mhmmdriz <rizki25030@gmail.com>
2024-11-09 20:04:30 +0700

    Create get book details feature

--- src/handler.js
+++ src/handler.js
@@ -83,7 +83,30 @@ const getAllBooksHandler = (request, h) => {
   return response;
 };
 
+const getBookDetails = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((n) => n.id === bookId)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
+  getBookDetails,
 };
